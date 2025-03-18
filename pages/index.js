32368312461235ab95a85f1c124232989a02bc89
BruhBanana1 <bruhbanana.yt@gmail.com>
2025-03-17 21:02:45 -0600

    oh

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-import Head from 'next/head';
-import '../styles/style.css';
-
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>My Next.js App</title>
-            </Head>
-            <h1>Hello, Next.js!</h1>
-            <script src="/script.js" defer></script>
-        </>
-    );
+    return <h1>Hello, Next.js! 🚀</h1>;
 }
